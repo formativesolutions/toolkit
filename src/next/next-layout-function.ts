bcf6ac9081dfd8e96eb0b54e0f1cd08225d2a37d
Trevor Sears <trevor@trevorsears.com>
2023-01-19 11:21:43 -0500

    Fixed `NextLayoutFunction` and `NextLayoutProps` to more closely match the real types.

--- src/next/next-layout-function.ts
+++ src/next/next-layout-function.ts
@@ -3,21 +3,21 @@
  *     (https://formativesolutions.io/).
  * 5:31 PM -- January 8th, 2023
  * Project: @formativesolutions/toolkit
- * 
+ *
  * @formativesolutions/toolkit - A toolkit of functions and functionality for
  * building Formative websites.
  * Copyright (C) 2022 Formative Solutions
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -28,8 +28,8 @@ import type { NextLayoutProps } from "./next-layout-props.js";
 /**
  * The type of the default exported function from layout files in NextJS's
  * `app/` directory.
- * 
+ *
  * @see NextLayoutProps
  */
 export type NextLayoutFunction<T extends string = never> =
-	(params: NextLayoutProps<T>) => ReactNode;
+	(props: NextLayoutProps<T>) => ReactNode | Promise<ReactNode>;
--- src/next/next-layout-props.ts
+++ src/next/next-layout-props.ts
@@ -3,39 +3,39 @@
  *     (https://formativesolutions.io/).
  * 5:31 PM -- January 8th, 2023
  * Project: @formativesolutions/toolkit
- * 
+ *
  * @formativesolutions/toolkit - A toolkit of functions and functionality for
  * building Formative websites.
  * Copyright (C) 2022 Formative Solutions
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import type { ReactElement } from "react";
+import type { ReactNode } from "react";
 
 /**
  * The type of the single parameter of the default function exported from layout
  * files in NextJS's `app/` directory.
- * 
+ *
  * Contains a 'children' field, which represents the content unique to a given
  * page that is using this layout.
  * Also contains a 'params' field which represents the path parameters that Next
  * has extracted from the current route.
- * 
+ *
  * @see NextLayoutFunction
  */
 export type NextLayoutProps<T extends string = never> = {
-	children: ReactElement;
+	children: ReactNode;
 	params: Record<T, string>;
 };
